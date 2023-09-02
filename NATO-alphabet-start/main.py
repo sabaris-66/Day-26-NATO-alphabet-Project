77970576d40a68ee6791a0ca95e79bebc7259bb9
sabaris-66 <sabaris.elango@gmail.com>
2023-09-02 15:55:58 +0530

    Try and exception added

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -6,6 +6,12 @@ new_dict = {row.letter: row.code for index, row in new_data.iterrows()}
 #print(new_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("enter your name or any other word:").upper()
-for letter in user_input:
-    print(f"{letter} for {new_dict[letter]}")
+game_is_on = True
+while game_is_on:
+    user_input = input("enter your name or any other word:").upper()
+    try:
+        for letter in user_input:
+            print(f"{letter} for {new_dict[letter]}")
+    except KeyError:
+        print("Try to enter only alphabets, Please!")
+
